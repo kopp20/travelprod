eb7d652fbc1b49d7eb330e9ca31750d1a60c2050
kopp20 <56162079+kopp20@users.noreply.github.com>
2021-01-15 14:14:53 +0100

    update

--- src/app/layout/places-map/places-map.page.ts
+++ src/app/layout/places-map/places-map.page.ts
@@ -13,71 +13,58 @@ export class PlacesMapPage implements OnInit, OnDestroy {
 
   map: L.Map;
 
-  private locationSubs: Subscription;
+  //private locationSubs: Subscription;
 
   geolocationPoints: L.LatLngTuple[];
-  propertyList = [];
-  error: string;
-  constructor(private plcMapService : PlaceMapService) {
-  }
+  markerId: string = "8f79cb4f-f3d7-473d-9bfe-31c85766f4ae";
 
-  ngOnInit(){
-    this.locationSubs = this.plcMapService.locationChanged.subscribe(list => {
+  constructor(private plcMapService: PlaceMapService) {}
+
+  ngOnInit() {
+    /* this.locationSubs = this.plcMapService.locationChanged.subscribe(list => {
       this.geolocationPoints = list;
       console.log(list)
       this.leafletMap(list);
-    });
-    this.plcMapService.fetchPlaces();
-
+    }); */
+    /* this.plcMapService.fetchPlaces(); */
+    this.plcMapService.getMarkersbyTripId(this.markerId).subscribe(result => {
+      this.leafletMap(result);
+    })
     this.map = new L.Map('map');
   }
 
   ngOnDestroy() {
-    this.locationSubs.unsubscribe();
+    /* this.locationSubs.unsubscribe(); */
   }
 
   ionViewDidEnter() {
-    
-    this.map.setView([46.947684, 7.395939], 16);
+
+    const test = this.map.setView([46.947684, 7.395939], 16);
 
     L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=CLh9srebP4AGiZBhlqC1Ru2R0EuX5ywlP45a9Pm01VP3mdImGZz2rqdP2deHhGaq', {
-	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	accessToken: 'CLh9srebP4AGiZBhlqC1Ru2R0EuX5ywlP45a9Pm01VP3mdImGZz2rqdP2deHhGaq'
-}).addTo(this.map);
-
-   
-
+      attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      accessToken: 'CLh9srebP4AGiZBhlqC1Ru2R0EuX5ywlP45a9Pm01VP3mdImGZz2rqdP2deHhGaq'
+    }).addTo(this.map);
   
-        // Make an HTTP request to retrieve the trips.
-        /* const url = `${environment.apiUrl}/places`;
-        this.http.get(url).subscribe((data) => {
-          console.log(data); */
-          
-          /* this.propertyList = data.properties; */
-        /* this.leafletMap(); */
-      /* }),
-      err => {
-        // Set the error information to display in the template
-        this.error = `An error occurred, the data could not be retrieved: Status: ${err.status}, Message: ${err.statusText}`;
-      } */
-
-      
 
 
   }
 
- myIcon = L.icon({
+
+  myIcon = L.icon({
     iconUrl: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
 
-    iconSize:     [38, 40], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    iconSize: [38, 40], // size of the icon
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
 
-  leafletMap(locationPoints:L.LatLngTuple[]) {
+  
+
+  leafletMap(locationPoints: L.LatLngTuple[]) {
     for (const point of locationPoints) {
-      L.marker(point, {icon: this.myIcon}).addTo(this.map)
+      L.marker(point, { icon: this.myIcon }).addTo(this.map).bindPopup("popupContent")
     }
   }
 
@@ -85,26 +72,5 @@ export class PlacesMapPage implements OnInit, OnDestroy {
     //this.map.remove();
   }
 
-  }
-  
+}
 
-  /* ngOnInit() {
-    this.map = L.map('map', {
-      center: [46.947673, 7.395961],
-      zoom: 25,
-      renderer: L.canvas()
-  })
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      // maxZoom: 12,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(this.map)
-
-    setTimeout(() => {
-      this.map.invalidateSize();
-    }, 0);
-
-
-    
-  }
-
-} */
--- src/app/layout/places-map/places-map.service.ts
+++ src/app/layout/places-map/places-map.service.ts
@@ -2,25 +2,32 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
 import {map} from 'rxjs/operators';
-import{Subject} from 'rxjs';
+import{Observable, Subject} from 'rxjs';
 import { LatLngTuple } from "leaflet";
 
 @Injectable({providedIn:'root'})
 export class PlaceMapService {
 
-    locationChanged = new Subject<LatLngTuple[]>();
+    //locationChanged = new Subject<LatLngTuple[]>();
 
-    list: string[];
+    //list: string[];
 
     constructor(private http: HttpClient){}
 
-    fetchPlaces(){
-        this.http.get<any>(`${environment.apiUrl}/places`).pipe(map(resData =>{
+    getMarkersbyTripId(id?:string){
+
+       
+        return this.http.get<any>(`${environment.apiUrl}/places`).pipe(map(resData =>{
+            return resData.filter(place => place.tripId === id).map(place => place.location.coordinates)
+        }));
+
+
+        /* this.http.get<any>(`${environment.apiUrl}/places`).pipe(map(resData =>{
             return resData.map(place => place.location.coordinates)
         }))
         .subscribe(transformedData => {
             console.log(transformedData);
             this.locationChanged.next(transformedData);
-        });
+        }); */
     }
 }
