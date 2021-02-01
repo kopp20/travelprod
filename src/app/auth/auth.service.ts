563aa6018c59d3017cf07182a1584a3daa1c3ff9
kopp20 <56162079+kopp20@users.noreply.github.com>
2021-02-01 15:06:54 +0100

    up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -52,7 +52,7 @@ export class AuthService {
 
   logIn(authRequest: AuthRequest): Observable<User> {
 
-    const authUrl = `${environment.apiUrl}auth`;
+    const authUrl = `https://kasslaprod-travel-log.herokuapp.com/api/auth`;
     return this.http.post<AuthResponse>(authUrl, authRequest).pipe(
       // Delay the observable stream while persisting the authentication response.
       delayWhen(auth => this.saveAuth(auth)),
@@ -66,7 +66,7 @@ export class AuthService {
 
 
   register(name: String, password: String) {
-    const UserUrl = `${environment.apiUrl}users`;
+    const UserUrl = `https://kasslaprod-travel-log.herokuapp.com/api/users`;
     return this.http.post<AuthResponse>(UserUrl,
       {name: name, password: password}
     )
