1c5dbaf1b3acabf3aacdbe7ec100944c62745f0e
Robin <robinbecherraz2@hotmail.fr>
2021-02-15 18:28:27 +0100

    Added environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrl: "https://kasslaprod-travel-log.herokuapp.com/api",
+    imgurApiUrl: "https://api.imgur.com/3",
+    imgurPwd: "3ns9bSt32gDh7bK",
+    imgurId: "3e43ecf6650451f",
+    imgurSecret: "dcb0e7f5b9ff2cfa6dae38f9a92d1dd64b876352",
+};
